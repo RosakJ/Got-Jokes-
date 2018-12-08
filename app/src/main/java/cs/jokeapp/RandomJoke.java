1c8da93c3b707e4e0fd9f3c341eeac5556bf143f
John Rosak <rosak2@illinois.edu>
2018-12-08 02:26:16 -0600

    Cleaned up the messy code in RandomJoke.java and put in necessary comments.

--- app/src/main/java/cs/jokeapp/RandomJoke.java
+++ app/src/main/java/cs/jokeapp/RandomJoke.java
@@ -3,10 +3,7 @@ package cs.jokeapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,19 +17,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.json.*;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import static com.android.volley.Request.Method.GET;
 
 public class RandomJoke extends AppCompatActivity {
 
@@ -50,7 +36,7 @@ public class RandomJoke extends AppCompatActivity {
      * This configures the home button to take you back to the home page.
      */
     private void configureHomeButton() {
-        Button homerandom = (Button) findViewById(R.id.backrandom);
+        Button homerandom = findViewById(R.id.backrandom);
         homerandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,11 +46,10 @@ public class RandomJoke extends AppCompatActivity {
     }
 
     /**
-     * This sets up the random joke button, it calls the get joke function and that should work
-     * itself out.
+     * This sets up the random joke button, it calls the get joke function.
      */
     private void configureJokeButton() {
-        Button randomJoke = (Button) findViewById(R.id.newrandom);
+        Button randomJoke = findViewById(R.id.newrandom);
         randomJoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,74 +59,37 @@ public class RandomJoke extends AppCompatActivity {
         });
     }
 
-    /*
-    private void getJoke() {
-        try {
-            URL url = new URL("http://api.icndb.com/jokes/random");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("Get");
-
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("param1", "val");
-
-            con.setDoOutput(true);
-            DataOutputStream out = new DataOutputStream(con.getOutputStream());
-            out.writeBytes(ParameterStringBuilder.getParamString(parameters));
-            out.flush();
-            out.close();
-
-            //I'm not sure where this is supposed to go but I'm trying it right here:
-            int status = con.getResponseCode();
-            BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            setText(content.toString());
-
-        } catch (Exception e) {
-            setText("Uh Oh.");
-        }
-
-
-    }
-
-
-
-
-    /*
     /**
-     * This class gets the joke, it sets up the whole object request and volley system
-     * I'm not sure this is the best way to do it, I saw some people make separate
-     * java classes for this like a background function, but this should work...
+     * This class gets the joke, it sets up a volley request system and requests the
      *
      * After it makes the request it calls the setText() method and passes it the joke
-     * (I don't know how parsing or the json file works just yet, this is in theory)
+     *
      */
-
     private void getJoke() {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         try {
-            URL url = new URL("http://api.icndb.com/jokes/random");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            //Creates a new request for a JSONObject
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
             Request.Method.GET, //Request type
-                "http://api.icndb.com/jokes/random", //URL string
-                null, //put parameters here
+                "http://api.icndb.com/jokes/random", //URL String
+                null, //Put parameters here
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.i("Joke Site", "It's responding!" + response.toString());
                         try {
-                            JSONObject obj = response;
+                            //If this goes through then the app worked!
+                            //This will parse the JSONObject to extract the joke and set the text.
                             String joke = response.getJSONObject("value").getString("joke");
                             setText(joke);
                         } catch (JSONException e) {
+                            //If an error occurs this will make a toast or a little pop up message
+                            //The pop up message will last for a bit and then go away, it's called
+                            //a toast
+                            //This error message will occur if the parsing fails or another JSON
+                            //Exception occurs
                             Context context = getApplicationContext();
-                            CharSequence text = "An error occurred pre!";
-                            int duration = Toast.LENGTH_SHORT;
+                            CharSequence text = "An Error Occurred!";
+                            int duration = Toast.LENGTH_LONG;
                             Toast toast = Toast.makeText(context, text, duration);
                             toast.show();
                         }
@@ -150,7 +98,7 @@ public class RandomJoke extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(final VolleyError error) {
-                        Log.i("Joke Site", "It's coming up with an error..." + error.toString());
+                        //This will come up with a toast if an error in the Volly itself occurs.
                         setText(error.toString());
                         Context context = getApplicationContext();
                         CharSequence text = "An error occurred in post!";
@@ -171,7 +119,7 @@ public class RandomJoke extends AppCompatActivity {
      * @param joke This is the text passed to it, preferably a joke.
      */
     private void setText(String joke) {
-        TextView jokeArea = (TextView) findViewById(R.id.jokeArea);
+        TextView jokeArea = findViewById(R.id.jokeArea);
         jokeArea.setText(joke);
     }
 }
