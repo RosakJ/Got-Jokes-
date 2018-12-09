b2301bd5210175553c7ddc4224deeae71ff91dbc
rajeeshah <rajees2@illinois.edu>
2018-12-08 23:17:18 -0600

    configured home button on categories page

--- app/src/main/java/cs/jokeapp/CategoriesActivity.java
+++ app/src/main/java/cs/jokeapp/CategoriesActivity.java
@@ -1,11 +1,13 @@
 package cs.jokeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -14,15 +16,18 @@ public class CategoriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        configureHomeButton();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    /**
+     * This configures the home button to home page.
+     */
+    private void configureHomeButton() {
+        Button homeCategories = findViewById(R.id.home);
+        homeCategories.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(CategoriesActivity.this, MainActivity.class));
             }
         });
     }
